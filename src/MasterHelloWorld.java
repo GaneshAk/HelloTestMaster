7a951cbf7a2728cb6e7c817e5de46050ca8fffa2
ganesh <akulaganeshsantosh@gmail.com>
2016-11-24 16:47:54 -0500

    MasterHelloWorld
    This is master file all request will be merged with this

---
+++ src/MasterHelloWorld.java
@@ -0,0 +1,10 @@
+
+public class MasterHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is helloworld master test");
+
+	}
+
+}
